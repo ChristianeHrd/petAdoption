9ca95db659f0e3257d0a7ab79f2a9f95da803266
Christiane Harada <w0476240@nscc.ca>
2023-02-23 15:22:07 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { createContext, useEffect, useState } from 'react'
 import { AuthProvider } from './components/Auth/AuthProvider'
-import Navigation from './components/Navigation'
 import Header from './components/Header'
 import Home from './components/Home'
 import About from './components/About'
@@ -15,11 +14,7 @@ import Cats from './Cats'
 import Dogs from './Dogs'
 import PetDetails from './PetDetails'
 import Footer from './components/Footer'
-import Countries from './components/noUsing/Countries'
-import CountryDetails from './components/noUsing/CountryDetails'
-// import Pets from './Pets'
-// import Carousel from './components/noUsing/photos/Carousel3'
-// import { SignInMethod } from 'firebase/auth'
+
 
 // body: "grant_type=client_credentials&client_id=7mJAtEk3jGnD2imRkPG8ebaeREOSZgPHh4ZolCPX6TyzucQfFe&client_secret=5sYdCKxlBJQWPbH6TGmkSb0yGp66sqgbco2pwTns" ,
 const petFinderKey = '7mJAtEk3jGnD2imRkPG8ebaeREOSZgPHh4ZolCPX6TyzucQfFe';
@@ -59,22 +54,13 @@ function App() {
         <AuthPetContext.Provider value={accessToken}>
           <AuthProvider>
             <Header />
-            {/* <Navigation /> */}
-
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/cats" element={<Cats />} />
               <Route path="/dogs" element={<Dogs />} />
               <Route path='/petdetails' element={<PetDetails />} />
-              
-              
-              {/* <Route path='/carousel' element={<Carousel />}></Route> */}
-              {/* <Route path="/pets" element={<Pets />} /> */}
-              {/* <Route path="/countries" element={<Countries />}>
-                <Route path="details/:countryname" element={<CountryDetails />} />
-              </Route> */}
-
+              {/* <Route path="/underconstruction" element={<UnderConstruction />} /> */}
               <Route path="/admin" element={<ProtectedRoutes />}>
                 <Route path="" element={<Admin />} />
               </Route>
@@ -82,7 +68,6 @@ function App() {
               <Route path="/signup" element={<Signup />} />
               <Route path="/logout" element={<Logout />} />
             </Routes>
-
             <Footer />
           </AuthProvider>
         </AuthPetContext.Provider>
