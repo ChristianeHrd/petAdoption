4196d7e6047d0d82e4a87a2c720e96e235c19534
Christiane Harada <w0476240@nscc.ca>
2023-02-23 14:39:45 -0400

    deleted previous footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ function Home() {
     return (
         <>
             <main className="homepage">
-                {/* <p>Welcome to the NSCC Fullstack Program's Authenticated Starter Template. You can use this template as a kicking off point to buid something amazing and fun!</p> */}
                 <p>Help us reduce the number of animals in shelters and rescues one adoption at a time.
                     We connect vulnerable animals with their forever families.
                 </p>
@@ -12,6 +11,8 @@ function Home() {
                     <img className='homePetPhoto' alt='puppy' src="/puppy.jpg" />
                     <img className='homePetPhoto' alt='kitten' src="/kitten.jpg" />
                     <img className='homePetPhoto' alt='bunny' src="./bunny.jpg" />
+
+
                 </div>
                 {/* </div> */}
 
@@ -25,3 +26,10 @@ function Home() {
 
 
 export default Home;
+
+
+{/* <p>Adopting a rescue animal can truly change their life - and yours! When you adopt a rescue,
+you're giving a second chance to an animal who may have had a difficult start in life.
+These animals often have so much love to give, and they're incredibly grateful for
+the chance to be part of a loving family.
+</p> */}
