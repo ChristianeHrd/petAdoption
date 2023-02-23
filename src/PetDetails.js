9653eabf02a630cd8b6fa14adea5338997c908a0
Christiane Harada <w0476240@nscc.ca>
2023-02-23 14:00:11 -0400

    changed some css

--- src/PetDetails.js
+++ src/PetDetails.js
@@ -12,33 +12,31 @@ function PetDetails() {
                 <h1>Hi, my name is {details.name} </h1>
                 <p>{details.description} </p>
             </div>
-           
+
             <div className='petDetails'>
                 <p>
-                    About <br />
-                    Breed: {details.breeds.primary}<br />
-                    Mixed Breed: {details.breeds.mixed === 'true' ? 'Yes' : 'No'}<br />
-                    Age: {details.age} <br />
+                    <span className='stats'>Breed: </span> {details.breeds.primary}<br />
+                    <span className='stats'>Mixed Breed:</span>  {details.breeds.mixed === 'true' ? 'Yes' : 'No'} <br />
+                    <span className='stats'>Age:</span>  {details.age}  <br />
 
-                    Status: {details.status}<br />
-                    Size: {details.size}<br />
+                    <span className='stats'>Status:</span>  {details.status} <br />
+                    <span className='stats'>Size:</span>  {details.size} <br />
 
-                    Good with Children: {details.environment.children === 'true' ? 'Yes' : 'No'}<br />
-                    Good with Dogs: {details.environment.dogs === 'true' ? 'Yes' : 'No'}<br />
-                    Good with Cats: {details.environment.cats === 'true' ? 'Yes' : 'No'}<br />
+                    <span className='stats'>Good with Children: </span> {details.environment.children === 'true' ? 'Yes' : 'No'}<br />
+                    <span className='stats'>Good with Dogs: </span> {details.environment.dogs === 'true' ? 'Yes' : 'No'}<br />
+                    <span className='stats'>Good with Cats: </span> {details.environment.cats === 'true' ? 'Yes' : 'No'} <br />
 
-                    Spayed/Neutered: {details.attributes.spayed_neutered === 'true' ? 'Yes' : 'No'}<br />
-                    House Trained: {details.attributes.house_trained === 'true' ? 'Yes' : 'No'}<br />
-                    Special Needs: {details.attributes.special_needs === 'true' ? 'Yes' : 'No'}<br />
-
-                    <a href={details.url}>link</a> <br />
+                    <span className='stats'>Spayed/Neutered:</span>  {details.attributes.spayed_neutered === 'true' ? 'Yes' : 'No'} <br />
+                    <span className='stats'>House Trained: </span> {details.attributes.house_trained === 'true' ? 'Yes' : 'No'} <br />
+                    <span className='stats'>Special Needs: </span> {details.attributes.special_needs === 'true' ? 'Yes' : 'No'} <br />
                 </p>
                 {/* <img alt={details.name} src={details.photos[0].medium}></img> */}
                 <div className='sliderContainer'>
                     <ImageSlider photos={details.photos}></ImageSlider>
                 </div>
             </div>
-
+            <br />
+            < span className='emailInfo'>For more information please email: <a href={'mailto:' + details.contact.email}>{details.contact.email}</a></span><br />
 
         </div>
     )
