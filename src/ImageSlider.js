8980ec070d2774e135e0d13c6381a9c5e0a3010d
Christiane Harada <w0476240@nscc.ca>
2023-02-23 10:08:24 -0400

    updated imageSlider.js

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 function ImageSlider({ photos }) {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const length = 3;
 
     function goToPrevious() {
         const isFirstSlide = currentIndex === 0;
@@ -20,8 +19,6 @@ function ImageSlider({ photos }) {
         setCurrentIndex(slideIndex)
     }
 
-    console.log('yaaa', photos)
-
     const slideStyle = {
         width: '100%',
         height: '100%',
@@ -32,49 +29,14 @@ function ImageSlider({ photos }) {
         backgroundImage: `url(${photos[currentIndex].full}`
     }
 
-    // const sliderStyle = {
-    //     height: '100%',
-    //     position: 'relative'
-    // }
-
-    // const leftArrayStyle = {
-    //     position: 'absolute',
-    //     top: '50%',
-    //     transform: 'translate(0, -50%)',
-    //     left: '32px',
-    //     fontSize: '45px',
-    //     color: '#fff',
-    //     zIndex: 1,
-    //     cursor: 'pointer'
-    // }
-    // const rightArrayStyle = {
-    //     position: 'absolute',
-    //     top: '50%',
-    //     transform: 'translate(0, -50%)',
-    //     right: '32px',
-    //     fontSize: '45px',
-    //     color: '#fff',
-    //     zIndex: 1,
-    //     cursor: 'pointer'
-    // }
-    // const dotsStyleContainer = {
-    //     display: 'flex',
-    //     justifyContent: 'center'
-    // }
-    // const dotsStyle = {
-    //     margin: '10px 8px',
-    //     cursor: 'pointer',
-    //     fontSize: '10px'
-    // }
-
     return (
         <div className='sliderStyle'>
-            <div className= 'leftArrayStyle' onClick={goToPrevious}>❰</div>
+            <div className='leftArrayStyle' onClick={goToPrevious}>❰</div>
             <div className='rightArrayStyle' onClick={goToNext}>❱</div>
             <div style={slideStyle}></div>
             <div className='dotsStyleContainer'>
                 {photos.map((photo, slideIndex) => (
-                    <div key={slideIndex} className='dotsStyle' onClick={()=>goToSlide(slideIndex)}>⚫</div>))}
+                    <div key={slideIndex} className='dotsStyle' onClick={() => goToSlide(slideIndex)}>⚫</div>))}
             </div>
         </div>
     )
