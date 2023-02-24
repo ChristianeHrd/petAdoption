0d0052edbcd2fd2e78076f6e85ca6f28ab1b0d27
Christiane Harada <w0476240@nscc.ca>
2023-02-23 23:28:34 -0400

    created contactForm, changed some css

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,51 +1,56 @@
 function ContactForm() {
     return (
         <>
-            <div class="row">
-                <form class="col s12">
-                    <div class="row">
-                        <div class="input-field col s6">
-                            <input placeholder="Placeholder" id="first_name" type="text" class="validate"/>
-                                <label for="first_name">First Name</label>
-                        </div>
-                        <div class="input-field col s6">
-                            <input id="last_name" type="text" class="validate" />
-                                <label for="last_name">Last Name</label>
+            <div className="contactForm">
+                <h1>Let's Get In Touch</h1>
+                <div className="contact-flex-container">
+                    <div>
+                        <div class="row">
+                            <form class="col s12">
+                                <div class="row">
+                                    <div class="input-field col s6">
+                                        <i class="material-icons prefix">account_circle</i>
+                                        <input id="first_name" type="text" />
+                                        <label for="first_name">First Name</label>
+                                    </div>
+                                    <div class="input-field col s6">
+                                        <input id="last_name" type="text" />
+                                        <label for="last_name">Last Name</label>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <i class="material-icons prefix">email</i>
+                                        <input id="email" type="email" class="validate" />
+                                        <label for="email">Email</label>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <form class="col s12">
+                                        <div class="row">
+                                            <div class="input-field col s6">
+                                                <i class="material-icons prefix">textsms</i>
+                                                <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+                                                <label for="icon_prefix2">Your message</label>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                <button class="btn waves-effect blue-grey darken-2" type="submit" name="action">Submit
+                                    <i class="material-icons right">send</i>
+                                </button>
+                            </form>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input disabled value="I am not editable" id="disabled" type="text" class="validate"/>
-                                <label for="disabled">Disabled</label>
-                        </div>
+                    <div>
+                        <img className="formPhoto" alt='cat' src="/sleepingCat.jpg" />
                     </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input id="password" type="password" class="validate"/>
-                                <label for="password">Password</label>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="input-field col s12">
-                            <input id="email" type="email" class="validate"/>
-                                <label for="email">Email</label>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col s12">
-                            This is an inline input field:
-                            <div class="input-field inline">
-                                <input id="email_inline" type="email" class="validate"/>
-                                    <label for="email_inline">Email</label>
-                                    <span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                </div>
             </div>
         </>
     )
-
 }
 
 export default ContactForm
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,20 @@
-function Home() 
-{
+import ContactForm from "./ContactForm";
+
+function Home() {
     return (
         <>
             <main className="homepage">
-                <p>Help us reduce the number of animals in shelters and rescues one adoption at a time.
-                    We connect vulnerable animals with their forever families.
-                </p>
+                <div className="homeParagrap">
+                    <p>Help us reduce the number of animals in shelters and rescues one adoption at a time.
+                        We connect vulnerable animals with their forever families.
+                    </p>
+                </div>
                 <div className="home-photo-flex-container">
                     <img className='homePetPhoto' alt='puppy' src="/puppy.jpg" />
                     <img className='homePetPhoto' alt='kitten' src="/kitten.jpg" />
                     <img className='homePetPhoto' alt='bunny' src="./bunny.jpg" />
                 </div>
+                <ContactForm />
             </main>
         </>
     )
